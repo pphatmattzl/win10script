38e9d264a6c69bdb9701433350b6c4fd110a422d
pphatmattzl <matt.zell@gmail.com>
2020-10-07 10:42:35 -0700

    Update Short Links

--- InstallApps&Tweaks.ps1
+++ InstallApps&Tweaks.ps1
@@ -12,7 +12,7 @@
 #
 #	Addition: One command to rule them all, One command to find it, and One command to Run it!
 #
-#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JUFJK')"
+#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JUpyR')"
 #
 #	Chris Titus Additions:
 #
