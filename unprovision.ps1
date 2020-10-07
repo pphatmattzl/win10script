103f58bca82ed7ad2000be6ba06c8733fa52b63f
pphatmattzl <matt.zell@gmail.com>
2020-10-07 12:08:21 -0700

    Update unprovision.ps1

--- unprovision.ps1
+++ unprovision.ps1
@@ -8,33 +8,22 @@ $tweaks = @(
 
 function Unprovision {
 	Write-Output "Uninstalling default third party applications..."
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.BingWeather_4.8.19002.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.GetHelp_10.2004.31291.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.Getstarted_9.10.32461.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.Microsoft3DViewer_2020.2009.29132.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.MicrosoftOfficeHub_18.2008.12711.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.MicrosoftSolitaireCollection_4.7.8101.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.MixedReality.Portal_2000.20081.1312.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.MSPaint_2020.2009.30067.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.Office.OneNote_16001.13127.20190.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.People_2020.324.807.0_neutral_~_8wekyb3d8bbwe"
-  Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.SkypeApp_15.64.80.0_neutral_~_kzf8qxf38zg5c"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.StorePurchaseApp_12009.1001.113.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.VCLibs.140.00_14.0.27323.0_x64__8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.Wallet_2.4.18324.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.WindowsAlarms_2021.2007.3.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.WindowsCamera_2020.504.60.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.WindowsFeedbackHub_2019.1111.2029.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.WindowsMaps_2021.2008.0.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.WindowsSoundRecorder_2021.2008.0.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.Xbox.TCUI_1.24.10001.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.XboxApp_48.69.18001.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.XboxGameOverlay_1.54.4001.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.XboxIdentityProvider_12.67.21001.0_neutral_~_8wekyb3d8bbwe"
-	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.XboxSpeechToTextOverlay_1.21.13002.0_neutral_~_8wekyb3d8bbwe"
-  Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.YourPhone_2020.923.45.0_neutral_~_8wekyb3d8bbwe"
-  Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.ZuneMusic_2019.20082.10421.0_neutral_~_8wekyb3d8bbwe"
-  Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.ZuneVideo_2019.20082.10421.0_neutral_~_8wekyb3d8bbwe"
+	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.549981C3F5F10_2.2009.23741.0_neutral_~_8wekyb3d8bbwe"
+	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.BingWeather_4.25.20211.0_neutral_~_8wekyb3d8bbwe"
+	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.DesktopAppInstaller_2019.1019.1.0_neutral_~_8wekyb3d8bbwe"
+	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.MicrosoftOfficeHub_18.1903.1152.0_neutral_~_8wekyb3d8bbwe"
+	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.MixedReality.Portal_2000.19081.1301.0_neutral_~_8wekyb3d8bbwe"
+	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.MSPaint_2019.729.2301.0_neutral_~_8wekyb3d8bbwe"
+	Remove-AppxProvisionedPackage -Online -PackageName "Microsoft.Office.OneNote_16001.12026.20112.0_neutral_~_8wekyb3d8bbwe"
+  Remove-AppxProvisionedPackage -Online -PackageName Microsoft.ScreenSketch_2019.904.1644.0_neutral_~_8wekyb3d8bbwe
+  Remove-AppxProvisionedPackage -Online -PackageName Microsoft.VP9VideoExtensions_1.0.32521.0_x64__8wekyb3d8bbwe
+  Remove-AppxProvisionedPackage -Online -PackageName Microsoft.WindowsCamera_2018.826.98.0_neutral_~_8wekyb3d8bbwe
+  Remove-AppxProvisionedPackage -Online -PackageName Microsoft.Xbox.TCUI_1.23.28002.0_neutral_~_8wekyb3d8bbwe
+  Remove-AppxProvisionedPackage -Online -PackageName Microsoft.XboxApp_48.49.31001.0_neutral_~_8wekyb3d8bbwe
+  Remove-AppxProvisionedPackage -Online -PackageName Microsoft.XboxGameOverlay_1.46.11001.0_neutral_~_8wekyb3d8bbwe
+  Remove-AppxProvisionedPackage -Online -PackageName Microsoft.XboxGamingOverlay_2.34.28001.0_neutral_~_8wekyb3d8bbwe
+  Remove-AppxProvisionedPackage -Online -PackageName Microsoft.XboxIdentityProvider_12.50.6001.0_neutral_~_8wekyb3d8bbwe
+  Remove-AppxProvisionedPackage -Online -PackageName Microsoft.XboxSpeechToTextOverlay_1.17.29001.0_neutral_~_8wekyb3d8bbwe
 
 }
 
